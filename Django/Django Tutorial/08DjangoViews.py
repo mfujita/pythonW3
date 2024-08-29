f1107b353aa68e113af9b7adbf052a87bc1c9916
Murilo Fujita <murilofujita@gmail.com>
2024-08-28 23:56:53 -0300

    Django Views

---
+++ Django/Django Tutorial/08DjangoViews.py
@@ -0,0 +1,4 @@
+# Django Views
+# Saão funções Python que aceitam HTTP request e retornam HTTP response como documentos HTML.
+# Uma página web que usa Django é cheia de views com diferentes tarefas e missões (tasks and missions).
+# As views geralmente são colocadas em arquivos chamados views.py e são localizadas no diretório do app.
