89c99cf43ce4c6eb361fbddc2dc78e26393777ab
Murilo Fujita <murilofujita@gmail.com>
2024-09-06 22:14:24 -0300

    Pie Charts

---
+++ Matplotlib/12PieCharts.py
@@ -0,0 +1,13 @@
+# Pie Charts
+# Creating Pie Charts → função pie()
+'''
+import matplotlib.pyplot as plt
+numeros=[16,8,4,2]
+plt.pie(numeros)
+plt.show()
+'''
+
+# O gráfico de pizza inicia no eixo X e é desenhado no sentido antihorário.
+# O tamanho de cada wedge é calculado através da fórmual x/sum(x).
+
+# Labels → para cada valor, um label descreve o wedge.
