a6e0cbde3daaf8e0a7f2932eb1e3ad0a639ec27e
Murilo Fujita <murilofujita@gmail.com>
2024-09-22 22:10:12 -0300

    Exercices with datetime

--- codigos/datetime1.py
+++ codigos/datetime1.py
@@ -5,3 +5,15 @@ mes = str(datetime.datetime.now()).split(' ')[0].split('-')[1]
 ano = str(datetime.datetime.now()).split(' ')[0].split('-')[0]
 print(dia+"/"+mes+"/"+ano)
 
+dataString = ['05/09/2024', '01/09/2024', '20/08/2024', '17/09/2024', '14/09/2024', '03/09/2024', '11/09/2024']
+myDate = []
+
+for i in range(len(dataString)):
+    dia=int(dataString[i].split('/')[0])
+    mes=int(dataString[i].split('/')[1])
+    ano=int(dataString[i].split('/')[2])
+    myDate.append(datetime.date(ano, mes, dia))
+
+myDate.sort()
+for item in myDate:
+    print(item)
