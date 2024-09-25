b6932a294a2eb793fa43a0627cafd26d30b5eb76
Murilo Fujita <murilofujita@gmail.com>
2024-09-24 23:01:06 -0300

    Exercise about read CSV

---
+++ codigos/readCSV.py
@@ -0,0 +1,5 @@
+import pandas as pd
+print('Site IBGE - aniversário dos municípios')
+df = pd.read_csv('C:\\Users\\Murilo\\Downloads\\aniversariantes.csv')
+print(df)
+# print(pd.options.display.max_rows)
