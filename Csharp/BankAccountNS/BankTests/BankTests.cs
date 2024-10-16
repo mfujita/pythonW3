68b8e61062d8282658e5d9f07f7cb9d71b0d94e6
Murilo Fujita <murilofujita@gmail.com>
2024-10-16 16:27:13 -0300

    Teste unitário exemplo

---
+++ Csharp/BankAccountNS/BankTests/BankTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using BankAccountNS;
+
+namespace BankTests
+{
+    [TestClass]
+    public class BankTests
+    {
+        [TestMethod]
+        public void TestDebit()
+        {
+            //Arrange
+            double saldoInicial = 110;
+            double debito = 100;
+            double saldoEsperado = 10;
+            BankAccount conta = new BankAccount("Saldo do cliente ", saldoInicial);
+
+            //Act
+            conta.Debit(debito);
+
+            //Assert
+            double saldoAtual = conta.Balance;
+            Assert.AreEqual(saldoEsperado, saldoAtual, 0.0001, "Erro na operação de bébito.");
+        }
+    }
+}
